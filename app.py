413d031800a554e69e6377a825a6a97114f2b587
shivam malhotra <141710462+Zupinn18@users.noreply.github.com>
2024-05-14 10:52:28 +0530

    U

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify, render_template
 from pymongo import MongoClient
-from bson import ObjectId
+from bson import ObjectId, json_util
 import json
 import pandas as pd
 
 app = Flask(__name__)
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
 
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -36,11 +39,14 @@ def get_data():
         collection = db[collection_name]
         data = collection.find_one()  # Assuming you want to fetch one document
         if data:
-            response_data[token] = {'status': 'success', 'data': data}
+            # Use json_util to parse MongoDB data (if needed)
+            parsed_data = parse_json(data)
+            response_data[token] = {'status': 'success', 'data': parsed_data}
         else:
             response_data[token] = {'status': 'error', 'message': 'Data not found'}
 
     return jsonify(response_data), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8009)
